3f2c52e90104c478060afa66601ad00915001d77
krishpatel20217 <krishpatel20217@gmail.com>
2026-07-31 22:56:10 +0530

    Initial commit: auth, accounts, email service setup

---
+++ src/models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
+
+const userSchema =new mongoose.Schema({
+    email :{
+        type:String,
+        require:[true,"Email is required for creating a user"],
+        trim:true,
+        lowercase:true,
+        match:[/^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/,"Invalid Email address"],
+        unique:[true,"Email already exist"]
+    },
+    name:{
+        type: String,
+        require:[true,"Name is require for creating an account"]
+    },
+    password:{
+        type:String,
+        require:[true,"Password is require for creating an account"],
+        minlength:[6,"password should contain more than 6 character"],
+        select:false
+    }
+},{
+    timestamps:true
+})
+
+userSchema.pre("save",async function(){
+    if(!this.isModified("password")){
+        return 
+    }
+    const hash = await bcrypt.hash(this.password , 10)
+    this.password = hash
+
+    return 
+})
+
+userSchema.methods.comparePassword = async function (password){
+    return await bcrypt.compare(password,this.password)
+}
+
+const userModel = mongoose.model("user",userSchema)
+
+module.exports = userModel
