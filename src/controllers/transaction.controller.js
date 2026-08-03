0bbe4c817d7a6837ca32ef1b5949c8926e8e4052
krishpatel20217 <krishpatel20217@gmail.com>
2026-08-03 14:03:02 +0530

    WIP: transaction transfer flow - step 2/10 (idempotency key check)

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -45,4 +45,39 @@ async function createTransaction(req,res){
             message:"invalid fromaccount or toaccount"
         })
     }
+
+    /**
+     * 2. Validate idempotency key
+     */
+
+    const isTransactionAlreadyExists = await transactionModel.findOne({
+        idempotencyKey:idempotencyKey
+    })
+
+    if(isTransactionAlreadyExists){
+        if(isTransactionAlreadyExists.status ==="COMPLETED"){
+            return res.status(200).json({
+                message:"Transaction already processed",
+                transaction: isTransactionAlreadyExists
+            })
+        }
+
+        if(isTransactionAlreadyExists.status ==="PENDING"){
+            return res.status(200).json({
+                message:"Transaction is still processing"
+            })
+        }
+
+        if(isTransactionAlreadyExists.status ==="FAILED"){
+            return res.status(500).json({
+                message:"Transaction processing failed , please retry"
+            })
+        }
+
+        if(isTransactionAlreadyExists.status ==="REVERSED"){
+            return res.status(500).json({
+                message:"Transaction is reversed, please retry"
+            })
+        }
+    }
 }
