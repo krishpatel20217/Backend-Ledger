da7f3f167b1b921a77ca4e543deb0a604bf14bbc
krishpatel20217 <krishpatel20217@gmail.com>
2026-08-05 20:04:53 +0530

    feat(accounts): add endpoint to fetch authenticated user's accounts

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -13,7 +13,18 @@ async function createAccountController(req,res) {
     })
 }
 
+async function getUserAccountsController(req,res){
+    const accounts = await accountModel.find({
+        user:req.user._id
+    })
+
+    res.status(200).json({
+        accounts
+    })
+}
+
 
 module.exports = {
-    createAccountController
+    createAccountController,
+    getUserAccountsController
 }
--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -15,4 +15,12 @@ const router = express.Router()
 router.post("/",authMiddleware.authMiddleware,accountController.createAccountController)
 
 
+/**
+ * - GET /api/accounts/
+ * - Get all accounts for the logged-in user
+ * - Protected Route
+ */
+router.get("/",authMiddleware.authMiddleware,accountController.getUserAccountsController)
+
+
 module.exports = router;
